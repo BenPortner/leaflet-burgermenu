c264d9ec2188b9c600fff3ce1cee736f4cabd962
Benjamin W. Portner <benjaminwportner@gmail.com>
2026-02-09 14:56:40 +0100

    add typescript declaration file

---
+++ src/leaflet-burgermenu.d.ts
@@ -0,0 +1,27 @@
+import { Control } from 'leaflet';
+
+declare module 'leaflet-burgermenu' {
+    export class BurgerMenuControl extends Control {
+        constructor(options?: BurgerMenuControlOptions);
+    }
+
+    export interface MenuItemWithClick {
+        title: string;
+        onClick: () => void;
+    }
+
+    export interface MenuItemWithSubItems {
+        title: string;
+        subItems: MenuItem[];
+    }
+
+    export type MenuItem = MenuItemWithClick | MenuItemWithSubItems;
+
+    export interface BurgerMenuControlOptions {
+        position?: 'topleft' | 'topright' | 'bottomleft' | 'bottomright';
+        menuIcon?: string;
+        subMenuIndicator?: string;
+        title?: string;
+        menuItems?: MenuItem[];
+    }
+}
